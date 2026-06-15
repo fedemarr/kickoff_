5bb567fbad5398af24a5eec4308c62fc1bd35297
Federico Martinez <fedemarr83@gmail.com>
2026-06-15 18:26:55 -0300

    fix: force-dynamic home page and remove NEXTAUTH_URL override

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -7,7 +7,7 @@ import { StatsBar } from '@/components/store/StatsBar'
 import { BrandsCarousel } from '@/components/store/BrandsCarousel'
 import { TestimonialsSlider } from '@/components/store/TestimonialsSlider'
 
-export const revalidate = 60
+export const dynamic = 'force-dynamic'
 
 async function getHomeProducts() {
   try {
